f6161d747aac0b0fe61b27ea4cdc448341d33cff
Hfear <hannahbfearns@gmail.com>
2025-04-28 22:07:49 -0400

    Spring boot integration worksgit add .git add .!

--- src/main/java/Application/Application.java
+++ src/main/java/Application/Application.java
@@ -3,8 +3,11 @@ package Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@ComponentScan(basePackages = {"Application", "Controller"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
