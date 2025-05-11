03a14d4f8d5762da618d074153569e7cce362b44
LoayE <elasmarloay@gmail.com>
2025-05-10 22:57:19 -0400

    Class view looks better

--- edu-frontend/src/ClassView.jsx
+++ edu-frontend/src/ClassView.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './ClassView.css';
 
 export default function ClassView({ singleClass }) {
     if (!singleClass) return null;
 
     return (
-        <li style={{ marginBottom: 12 }}>
-            <Link
-                to={`/class/${singleClass.code}`}
-                style={{ textDecoration: 'none', color: '#1e90ff' }} // changed color
-            >
-                <strong>{singleClass.title}</strong>
-                <div style={{ fontSize: 14, color: '#555' }}>
-                    {singleClass.description}
+        <li className="class-item">
+            <Link to={`/class/${singleClass.code}`} className="class-link">
+                <div className="class-item__content">
+                    {singleClass.title}: {singleClass.description}
                 </div>
             </Link>
         </li>
